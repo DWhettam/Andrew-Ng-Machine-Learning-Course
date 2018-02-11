0d74339a894a09065fcfbad763d33a3a5d8939e4
Dan Whettam <33871216+DWhettam@users.noreply.github.com>
2018-02-11 13:49:01 +0000

    Adjust centroids to mean of training values

--- ex7/ex7/computeCentroids.m
+++ ex7/ex7/computeCentroids.m
@@ -25,11 +25,10 @@ centroids = zeros(K, n);
 %
 % Note: You can use a for-loop over the centroids to compute this.
 %
-
-
-
-
-
+for k = 1:K
+  sel = find(idx == k)
+  centroids(k,:) = mean(X(sel,:));
+endfor
 
 
 
